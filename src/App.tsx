9ab2e389da475fc47c42c8ab18025aa46dccb5e9
David Campos <david.campos.solorio@gmail.com>
2023-01-26 17:46:25 -0600

    react router v6 setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Navigation } from './routes/Navigation'
+
+function App() {
+    return (
+       <Navigation />
+    )
+}
+
+export default App
