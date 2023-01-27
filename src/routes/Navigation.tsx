8657f33dd2870b1c1e5f2fa73b0828b3ea3ef06e
David Campos <david.campos.solorio@gmail.com>
2023-01-26 18:32:21 -0600

    react router v5 setup

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,31 +1,37 @@
-import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import logo from '../assets/react.svg';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-    <div className="main-layout">
-        <nav>
-            <img src={logo} alt="React Logo" />
-            <ul>
-                <li>
-                    <NavLink to='/' className={ ({ isActive }) => isActive ? 'nav-active' : ''}> Home </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about' className={ ({ isActive }) => isActive ? 'nav-active' : ''}> About </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/users' className={ ({ isActive }) => isActive ? 'nav-active' : ''}> Users </NavLink>
-                </li>
-            </ul>
-        </nav>
-        <Routes>
-            <Route path='about' element={<h1>AboutPage</h1>} />
-            <Route path='users' element={<h1>UsersPage</h1>} />
-            <Route path='/' element={<h1>HomePage</h1>} />
-            <Route path='*' element={<Navigate to='/' replace />} />
-        </Routes>
-    </div>
-    </BrowserRouter>
+    <Router>
+        <div className="main-layout">
+            <nav>
+                <img src={logo} alt="React Logo" />
+                <ul>
+                    <li>
+                        <NavLink to='/' activeClassName='nav-active' exact> Home </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about' activeClassName='nav-active' exact> About </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/users' activeClassName='nav-active' exact> Users </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path='/about'>
+                    <h1>AboutPage</h1>
+                </Route>
+                <Route path='/users'>
+                    <h1>UsersPage</h1>
+                </Route>
+                <Route path='/'>
+                    <h1>HomePage</h1>
+                </Route>
+                {/* <Route path='*' element={<Navigate to='/' replace />} /> */}
+            </Switch>
+        </div>
+    </Router>
   )
 }
